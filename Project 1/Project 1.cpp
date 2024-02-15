5e73b15acfba6587c0676e379660a2e6c07d6e4e
Abubakr <thefox5000@gmail.com>
2024-02-15 10:11:42 +0300

    Create Size method

--- Project 1/Project 1.cpp
+++ Project 1/Project 1.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-#include "MydbLinkedList.h"
+#include "clsDblLinkedList.h"
 
 using namespace std;
 
 int main()
 {
-    MydbLinkedList<int> MyList;
+    clsDblLinkedList<int> MyList;
 
     MyList.InsertAtEnd(1);
     MyList.InsertAtEnd(2);
     MyList.InsertAtEnd(3);
 
-    MydbLinkedList<int>::Node *N1 = MyList.Find(2);
+    clsDblLinkedList<int>::Node *N1 = MyList.Find(2);
 
     MyList.InsertAfter(N1, 20);
     
     MyList.PrintList();
+
+    cout << "Size is:" << MyList.Size() << endl;
 }
