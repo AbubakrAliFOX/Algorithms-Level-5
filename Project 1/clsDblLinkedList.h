66d1c4711551f245c1bacdf8e10ce0022bdc491f
Abubakr <thefox5000@gmail.com>
2024-02-15 10:46:37 +0300

    Reverse List

--- Project 1/clsDblLinkedList.h
+++ Project 1/clsDblLinkedList.h
@@ -218,15 +218,34 @@ public:
         return _Size;
     }
 
-    bool IsEmpty () {
+    bool IsEmpty()
+    {
         return _Size == 0;
     }
 
-    void Clear () {
+    void Clear()
+    {
         while (_Size > 0)
         {
             DeleteFirstNode();
         }
-        
+    }
+
+    void Reverse()
+    {
+        Node *current = head;
+        Node *temp = nullptr;
+        while (current != nullptr)
+        {
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+            current = current->prev;
+        }
+
+        if (temp != nullptr)
+        {
+            head = temp->prev;
+        }
     }
 };
