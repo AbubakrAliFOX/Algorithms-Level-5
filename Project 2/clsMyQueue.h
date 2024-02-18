1a55b414e091a6ca05f2fe6426d72a336740f8ee
Abubakr <thefox5000@gmail.com>
2024-02-18 09:35:07 +0300

    Finish Project 2

--- Project 2/clsMyQueue.h
+++ Project 2/clsMyQueue.h
@@ -1,9 +1,43 @@
 #pragma once
 #include "clsDblLinkedList.h"
+#include <iostream>
+using namespace std;
 
-template<typename T>
-class clsMyQueue : public clsDblLinkedList<T> {
+template <typename T>
+class clsMyQueue
+{
+
+protected:
+    clsDblLinkedList <T> _List;
+
 public:
-    
-};
+    void push(T value)
+    {
+        _List.InsertAtEnd(value);
+    }
 
+    void Print()
+    {
+        _List.PrintList();
+    }
+
+    short Size()
+    {
+        return _List.Size();
+    }
+
+    T front()
+    {
+        return _List.GetItem(0);
+    }
+
+    T back()
+    {
+        return _List.GetItem(Size() - 1);
+    }
+
+    void pop()
+    {
+        _List.DeleteLastNode();
+    }
+};
